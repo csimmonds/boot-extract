1e2cfb893fa8a7b059513490e00cb42c0fc509d8
Chris Simmonds <chris@2net.co.uk>
2016-07-18 14:58:21 +0100

    Now writes out the second binary
    Add code to write out the second binary if its size is > 0
    The second binary is often used to store a device tree binary (dtb),
    hence the file is named second.dtb

--- boot-extract.c
+++ boot-extract.c
@@ -2,7 +2,7 @@
 /*                                                                           */
 /* Android Boot image extraction tool                                        */
 /*                                                                           */
-/* Copyright (C) 2012 Chris Simmonds                                         */
+/* Copyright (C) 2012,2016 Chris Simmonds                                    */
 /*                                                                           */
 /* This program is free software; you can redistribute it and/or modify      */
 /* it under the terms of the GNU General Public License as published by      */
@@ -37,26 +37,51 @@
 */
 #include "bootimg.h"
 
+extern int optind;
+
+static void usage(char *prog)
+{
+	printf("\nAndroid boot image extraction tool v1.1\n"
+	       "Copyright (C)2012,2016 Chris Simmonds\n\n"
+	       "Usage %s <-i> [boot or recovery image file}\n"
+	       "    -i   information only: do not extract images\n", prog);
+	exit(1);
+}
+
 int main(int argc, char **argv)
 {
+	int opt;
+	int info_only = 0;
 	int f;
 	int fk;
 	int fr;
 	int n;
 	char *buf;
-	unsigned int flash_page_size = 2048;
+	unsigned int flash_page_size;
 	struct boot_img_hdr hdr;
 
-	if (argc < 2) {
-		printf("\nAndroid boot image extraction tool v1.0\n");
-		printf("Copyright (C)2012,2016 Chris Simmonds\n\n");
-		printf("Usage %s <boot or recovery image file>\n", argv[0]);
-		return 1;
-	}
+	if (argc == 1)
+		usage(argv[0]);
 
-	f = open(argv[1], O_RDONLY);
+	while ((opt = getopt(argc, argv, "hi")) != -1) {
+		switch (opt) {
+		case 'i':
+			info_only = 1;
+			break;
+		case 'h':
+			usage(argv[0]);
+		default:
+			exit(1);
+		}
+	}
+	printf("argc %d optind %d\n", argc, optind);
+
+	if (optind == argc)
+		usage(argv[0]);
+
+	f = open(argv[optind], O_RDONLY);
 	if (f == -1) {
-		printf("Failed to open %s (%s)\n", argv[1], strerror(errno));
+		printf("Failed to open %s (%s)\n", argv[optind], strerror(errno));
 		return 1;
 	}
 
@@ -87,6 +112,9 @@ int main(int argc, char **argv)
 	       hdr.ramdisk_size, hdr.ramdisk_addr,
 	       hdr.second_size, hdr.second_addr,
 	       hdr.tags_addr, hdr.name, hdr.cmdline);
+
+	if (info_only)
+		return 0;
 
 	flash_page_size = hdr.page_size;
 	if (hdr.kernel_size > 0) {
@@ -144,5 +172,37 @@ int main(int argc, char **argv)
 		close(fr);
 		printf("ramdisk.cpio.gz extracted\n");
 	}
+	if (hdr.second_size > 0) {
+		int second_offset;
+
+		/* extract second binary */
+		second_offset =
+		    (((hdr.kernel_size + hdr.ramdisk_size + flash_page_size -
+		       1) / flash_page_size) + 1) * flash_page_size;
+		printf("Second binary at offset %d (0x%x)\n", second_offset,
+		       second_offset);
+
+		lseek(f, second_offset, SEEK_SET);
+		fr = open("second.dtb", O_WRONLY | O_CREAT | O_TRUNC, 0644);
+		if (fr < 0) {
+			printf("Failed to create second (dtb) file\n");
+			return 1;
+		}
+		buf = malloc(hdr.second_size);
+		if (buf == 0) {
+			printf("malloc failed\n");
+			return 1;
+		}
+		n = read(f, buf, hdr.second_size);
+		if (n != hdr.second_size) {
+			printf ("Error in read\n");
+			return 1;
+		}
+		write(fk, buf, hdr.second_size);
+		free(buf);
+		close(fr);
+		printf("second.dtb extracted\n");
+	}
+
 	return 0;
 }
